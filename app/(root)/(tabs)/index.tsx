b52454b9a84fcb4c8d4f9e406b7e4d58717c5906
07amansaini8684 <mr.amannobody@gmail.com>
2025-01-04 23:39:38 +0530

    small but imp - FlatList op, remove the isLoggedIn cuz... h kuch bat

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,57 +1,82 @@
-import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
-import {Link} from "expo-router";
-import {EXPO_PUBLIC_APPWRITE_PROJECT_ID} from "@env";
+import { Link } from "expo-router";
+
 
 
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
-import {Card, FeaturedCard} from "@/components/Cards";
+import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
+import { FlatList } from 'react-native';
+import { useGlobalContext } from '@/lib/golabal-provider';
 const index = () => {
 
-  return (
-    <SafeAreaView className="bg-whtie -h-full">
-        <View className="px-5">
-            <View className="flex flex-row items-center justify-between mt-5">
-                <View className="flex flex-row items-center">
-                    <Image source={images.avatar} className="size-10 roundede-lg" resizeMode="contain" />
-                    <View className="flex flex-col items-start  ml-2 justify-center" >
-                        <Text className="text-xs font-rubik text-black-100">Dattebayo</Text>
-                        <Text className="">Blue | Moon</Text>
+    const {user} = useGlobalContext()
+    console.log("user", user)
+
+    return (
+        <SafeAreaView className="bg-whtie -h-full">
+            <FlatList
+                data={[1, 2,3,4]}
+                renderItem={({ item }) => <Card/>}
+                keyExtractor={(item) => item.toString()}
+                numColumns={2}
+                contentContainerClassName='pb-32'
+                columnWrapperClassName='flex gap-5 px-5'
+                showsVerticalScrollIndicator={false}
+
+                ListHeaderComponent={
+                    <View className="px-5">
+                        <View className="flex flex-row items-center justify-between mt-5">
+                            <View className="flex flex-row items-center">
+                                <View className="bg-primary-300/70 w-12 h-12 rounded-full flex items-center justify-center overflow-hidden p-0.5">
+                                <Image source={user?.avatar ? {uri : user?.avatar} : images.realAvatar} className="w-full h-full rounded-full" resizeMode="contain" />
+                                </View>
+                                <View className="flex flex-col items-start  ml-2 justify-center" >
+                                    <Text className="text-xs font-rubik text-black-100">Dattebayo</Text>
+                                    <Text className="">{user?.name || "looker"}</Text>
+                                </View>
+                            </View>
+                            <Image source={icons.bell} className="size-6" resizeMode="contain" />
+                        </View>
+                        <Search />
+                        <View className="mt-5">
+                            <View className="flex flex-row items-center justify-between px-2">
+                                <Text className="text-black-300 font-rubik-semibold text-lg">Featured</Text>
+                                <TouchableOpacity>
+                                <Link href="/explore" className="text-primary-300 font-rubik text-sm">See All</Link>
+                                </TouchableOpacity>
+                            </View>
+                            <FlatList 
+                            data={[1, 2, 3, 4]}
+                            renderItem={({ item }) => <FeaturedCard/>}
+                            keyExtractor={(item) => item.toString()}
+                            horizontal 
+                            showsHorizontalScrollIndicator={false}
+                            contentContainerClassName='flex gap-5 mt-5'
+                            bounces={false}
+
+                            />
+                        </View>
+                        
+                        <View className="mt-5">
+                            <View className="flex flex-row items-center justify-between px-2">
+                                <Text className="text-black-300 font-rubik-semibold text-lg">Recommended</Text>
+                                <TouchableOpacity>
+                                    <Link href="/explore" className="text-primary-300 font-rubik text-sm">See All</Link>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <Filters />
+                     
                     </View>
-                </View>
-                <Image source={icons.bell} className="size-6" resizeMode="contain" />
-            </View>
-        <Search/>
-        <View className="mt-5">
-            <View className="flex flex-row items-center justify-between px-2">
-                <Text className="text-black-300 font-rubik text-lg">Featured</Text>
-                <Link href="/explore" className="text-primary-300 font-rubik text-sm">See All</Link>
-            </View>
-        </View>
-       <View className="flex flex-row gap-5 mt-5">
-           <FeaturedCard />
-           <FeaturedCard />
-           <FeaturedCard />
-       </View>
-            <View className="mt-5">
-                <View className="flex flex-row items-center justify-between px-2">
-                    <Text className="text-black-300 font-rubik text-lg">Recommended</Text>
-                    <TouchableOpacity>
-                        <Link href="/explore" className="text-primary-300 font-rubik text-sm">See All</Link>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <Filters/>
-            <View className="flex flex-row gap-5 mt-5">
-                <Card />
-                <Card />
-            </View>
-        </View>
-    </SafeAreaView>
-  )
+                }
+            />
+
+        </SafeAreaView>
+    )
 }
 
 export default index
